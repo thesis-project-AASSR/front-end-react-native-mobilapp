29fc377b94e91644fd966921333805b5171befde
AreenBadran <areenbadran9@gmail.com>
2020-12-30 15:20:13 +0200

    Get data from backend for the user profile using axios

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './components/Profile/profile'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
+      {console.log("hii")}
+      <Text>hello worlddduuuuu!</Text>
+      
+      <Profile/>
+      
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -19,3 +24,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import {createAppContainer} from 'react-navigation';
+// import {createDrawerNavigator} from 'react-navigation-drawer';
+// import {Dimensions} from 'react-native';
+// import {Feather} from '@expo/vector-icons';
+// import {ProfileScreen, MessageScreen,ActivityScreen} from './screens';
+
+// const DrawerNavigator = createDrawerNavigator({
+//   ProfileScreen,
+//   MessageScreen,
+//   ActivityScreen
+// })
+
+// export default createAppContainer(DrawerNavigator);
--- components/Profile/profile.js
+++ components/Profile/profile.js
@@ -1,18 +1,72 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
-import StyledButton from '../StyledButton';
-import styles from './style';
+import React, {useEffect, useState}from 'react';
+import {View, Text, ImageBackground, Image, StyleSheet} from 'react-native';
+// import StyledButton from '../StyledButton';
+// import styles from './style';
+import axios from 'axios';
 
-const Profile = (props) => {
+const Profile = () => {
 // const {name,tagline,image} = props;
 
+//to know which user's profile
+// var User=localStorage.getItem('user_id');
 
-    return (
-        <View >
+//hooks to set state
+const [profile, setProfile] = useState("");
+// console.log(profile);
+
+//to get the data directly
+useEffect( () => {
+    axios.get('http://localhost:5000/UserProfile')
+    .then(res => {
+        setProfile(res.data);
+        console.log(res.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+},[]);
+
+//to know which user
+//get id from local storage
+//var User = id from local storage
+//map over the array
+//if profile[i].userID === User
+//return object
+//loop over the object and render it's data
 
 
-        </View>
-    )
+//use filter an array of objects to get the user logged in
+//var User = id from local storage
+
+{Object.entries(profile).map(([key, v]) => {
+    return  <View key={key}>
+            <Text>
+
+            <Image 
+            source={{
+                uri: v.image,
+                }} 
+                />
+            <Text>username: {v.username}</Text>
+            <Text>email: {v.email}</Text>
+            <Text>phone number: {v.phoneNumber}</Text>
+
+            </Text>
+
+       </View>
+             
+})}
+
+
 }
 
 export default Profile;
+
+{/* <View>
+<Text>
+    <Image uri={person.image} />
+    <Text>{person.username}</Text>
+    <Text>{person.email}</Text>
+    <Text>{person.phoneNumber}</Text>
+</Text>
+</View> */}
