4b93c3c0426643214e622f2f2eb8bade5ab6d83f
AreenBadran <areenbadran9@gmail.com>
2020-12-30 22:55:48 +0200

    user profile done

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {console.log("hii")}
-      <Text>hello worlddduuuuu!</Text>
+      {/* <Text>hello worlddduuuuu!</Text> */}
       
       <Profile/>
       
--- components/Profile/profile.js
+++ components/Profile/profile.js
@@ -1,22 +1,25 @@
 import React, {useEffect, useState}from 'react';
-import {View, Text, ImageBackground, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 // import StyledButton from '../StyledButton';
 // import styles from './style';
 import axios from 'axios';
+// import AsyncStorage from '@react-native-community/async-storage';
 
 const Profile = () => {
-// const {name,tagline,image} = props;
-
-//to know which user's profile
-// var User=localStorage.getItem('user_id');
 
 //hooks to set state
 const [profile, setProfile] = useState("");
-// console.log(profile);
 
-//to get the data directly
+
+//to get the data directly ==> useEffect
+//to know which user
+//get id from local storage
+//var User = id from local storage
+//filter the array of objects to user id
+//map over the user's object and render it's data
+//
 useEffect( () => {
-    axios.get('http://localhost:5000/UserProfile')
+    axios.get('http://192.168.1.13:5000/UserProfile')
     .then(res => {
         setProfile(res.data);
         console.log(res.data);
@@ -26,47 +29,47 @@ useEffect( () => {
     })
 },[]);
 
-//to know which user
-//get id from local storage
-//var User = id from local storage
-//map over the array
-//if profile[i].userID === User
-//return object
-//loop over the object and render it's data
+//to know which user's profile get user id from local storage
+// var User=localStorage.getItem('user_id');
+
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    tinyLogo: {
+      width: 150,
+      height: 150,
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
 
 
-//use filter an array of objects to get the user logged in
-//var User = id from local storage
 
-{Object.entries(profile).map(([key, v]) => {
-    return  <View key={key}>
-            <Text>
+return (
+    <View>
+{Object.entries(profile).filter(([key, v]) => 
+    v.userID === 1).map(([key,v]) => {
+        return  <View key={key}>
+        <Text>
 
-            <Image 
-            source={{
-                uri: v.image,
-                }} 
-                />
-            <Text>username: {v.username}</Text>
-            <Text>email: {v.email}</Text>
-            <Text>phone number: {v.phoneNumber}</Text>
+        <Image style={styles.tinyLogo} source={{ uri: v.image,}}/>
+        <Text>username: {v.username}</Text>
+        <Text>email: {v.email}</Text>
+        <Text>phone number: {v.phoneNumber}</Text>
 
-            </Text>
+        </Text>
 
-       </View>
-             
-})}
+   </View>
 
+    })}
+
+     </View>
+)
 
 }
 
 export default Profile;
 
-{/* <View>
-<Text>
-    <Image uri={person.image} />
-    <Text>{person.username}</Text>
-    <Text>{person.email}</Text>
-    <Text>{person.phoneNumber}</Text>
-</Text>
-</View> */}
