119dfca8f649ee30506827604de5dac667fedb1e
saraalrajabii <saraalrajabii12@gmail.com>
2021-01-01 16:32:50 +0200

    chat-Feature- done

--- App.js
+++ App.js
@@ -1,38 +1,23 @@
 
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import Items from './components/List items/items'
-import Adding from './components/List items/addItem'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-export default function App() {
-  const Stack = createStackNavigator();
-  return (
 
-<NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="items" component={Items} />
-    <Stack.Screen name="addItems" component={Adding} />
-     
-     
-      
-    </Stack.Navigator>
-  </NavigationContainer>
-    
+import React  from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { navigationRef } from "./navigation/sara";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Main from './components/chat/Main'
+import Chat from './components/chat/Chat'
+
+export default function App() {
+
+  const Stack = createStackNavigator();
+
+  return (
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator mode="modal" >
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-{/* <View style={styles.container}>
-     
-     <Items/>
-      <StatusBar style="auto" />
-    </View> */}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+    import { SafeAreaView } from 'react-navigation'
+    import { Text, StyleSheet } from 'react-native'
+    // import Map from '../../components/map'
+
+    const MapScreen = ({ navigation }) => {
+        return (
+        <SafeAreaView forceInset={{ top: 'always' }}>
+        <Text >MapScreen</Text>
+        </SafeAreaView>
+    )
+    }
+
+    export default MapScreen;
